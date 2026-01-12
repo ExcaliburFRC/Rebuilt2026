ab00354cd67ad0d123ecd55e257fc0631fca5530
Yehuda Rothstein <129756006+YehudaRothstein@users.noreply.github.com>
2026-01-12 19:11:40 +0200

    pathplanner integration

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,18 +4,43 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
+import frc.excalib.control.math.Vector2D;
+import frc.excalib.swerve.Swerve;
 import monologue.Logged;
 
 public class RobotContainer implements Logged {
-  public RobotContainer() {
-    configureBindings();
-  }
+    CommandPS5Controller driver = new CommandPS5Controller(0);
 
-  private void configureBindings() {}
+    Swerve swerve = Constants.SwerveConstants.configureSwerve(new Pose2d());
 
-  public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
-  }
+    public RobotContainer() {
+        configureBindings();
+    }
+
+    private void configureBindings() {
+        driver.cross().onTrue(swerve.pathfindThenFollowPathCommand("Example Path"));
+
+         swerve.setDefaultCommand(
+                swerve.driveCommand(
+                        () -> new Vector2D(
+                                applyDeadband(-driver.getLeftY()) * Constants.SwerveConstants.MAX_VEL,
+                                applyDeadband(-driver.getLeftX()) * Constants.SwerveConstants.MAX_VEL),
+                        () -> applyDeadband(-driver.getRightX()) * Constants.SwerveConstants.MAX_OMEGA_RAD_PER_SEC,
+                        () -> true
+                )
+        );
+
+    }
+
+    public double applyDeadband(double val) {
+        return Math.abs(val) < Constants.DEADBAND_X ? 0 : val;
+    }
+
+    public Command getAutonomousCommand() {
+        return Commands.print("No autonomous command configured");
+    }
 }

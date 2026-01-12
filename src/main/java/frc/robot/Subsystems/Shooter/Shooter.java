f51ff17778859a64532f24cd4a18fbcf05008375
Software Dev <excalibur@ybalm.co.il>
2026-01-12 21:30:55 +0200

    Shooter warmup ,fuel transfer and shoot command. *without physics

--- src/main/java/frc/robot/Subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter/Shooter.java
@@ -2,15 +2,19 @@ package frc.robot.Subsystems.Shooter;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.excalib.control.limits.SoftLimit;
 import frc.excalib.control.motor.controllers.TalonFXMotor;
 import frc.excalib.mechanisms.Mechanism;
 import frc.excalib.mechanisms.fly_wheel.FlyWheel;
 
 import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
 
 import static frc.robot.Subsystems.Shooter.ShooterConstants.*;
 
@@ -21,13 +25,21 @@ public class Shooter extends SubsystemBase {
     public final FlyWheel flyWheelMechanism;
     public final Mechanism supportWheelMechanism, hoodMechanism;
     public final SoftLimit hoodSoftLimit;
+    public final TalonFXMotor transportMotor;
     public final DoubleSupplier hoodAngleSupplier;
+    public final DigitalInput beamBreak;
+    public final Trigger hasFuel;
+    public final Mechanism transportMechanism;
+
     public Shooter() {
         hoodMotor = new TalonFXMotor(HOOD_MOTOR_ID);
         flyWheelMotor = new TalonFXMotor(FLYWHEEL_MOTOR_ID);
         supportWheelMotor = new TalonFXMotor(SUPPORT_WHEEL_MOTOR_ID);
+        beamBreak = new DigitalInput(BEAM_BREAK_CHANNEL);
+        transportMotor = new TalonFXMotor(TRANSPORT_MOTOR_ID);
 
         hoodAngleSupplier = () -> (hoodMotor.getPosition().getValueAsDouble() * POSITION_CONVERSION_FACTOR);
+        transportMechanism = new Mechanism(transportMotor);
         hoodMechanism = new Mechanism(hoodMotor);
         flyWheelMechanism = new FlyWheel(flyWheelMotor, FLY_WHEEL_MAX_ACCELERATION, FLY_WHEEL_MAX_JERK, FLYWHEEL_GAINS);
         supportWheelMechanism = new Mechanism(supportWheelMotor);
@@ -35,6 +47,8 @@ public class Shooter extends SubsystemBase {
                 () -> HOOD_MIN_ANGLE_LIMIT,
                 () -> HOOD_MAX_ANGLE_LIMIT
         );
+        hasFuel = new Trigger(beamBreak::get);
+
     }
 
     public Command setHoodAngleCommand(DoubleSupplier angleSetpoint){
@@ -58,4 +72,26 @@ public class Shooter extends SubsystemBase {
                 }
         );
     }
+
+    public Command flyWheelWarmupCommand(double velocity) {
+        return flyWheelMechanism.smartVelocityCommand(() -> velocity, this);
+    }
+
+    public Command getFuelCommand() {
+        return new RunCommand(
+                () -> transportMechanism.setVoltage(TRANSPORT_VOLTAGE)
+        ).until(hasFuel.negate()).withTimeout(0.1);
+    }
+
+    public Command shootCommand() {
+        double angle = 0;
+        double flyWheelSpeed = 0;
+        return new SequentialCommandGroup(
+                flyWheelWarmupCommand(flyWheelSpeed),
+                setHoodAngleCommand(() -> angle),
+                getFuelCommand()
+        );
+    }
+
+
 }
--- src/main/java/frc/robot/Subsystems/Shooter/ShooterConstants.java
+++ src/main/java/frc/robot/Subsystems/Shooter/ShooterConstants.java
@@ -8,6 +8,8 @@ public class ShooterConstants {
     public static final int FLYWHEEL_MOTOR_ID = 0;
     public static final int SUPPORT_WHEEL_MOTOR_ID = 0;
     public static final int FLY_WHEEL_MAX_ACCELERATION = 0;
+    public static final int TRANSPORT_MOTOR_ID = 0;
+    public static final int TRANSPORT_VOLTAGE = 0;
     public static final int FLY_WHEEL_MAX_JERK = 0;
     public static final int HOOD_MIN_ANGLE_LIMIT = 0;
     public static final int HOOD_MAX_ANGLE_LIMIT = 0;
@@ -16,5 +18,6 @@ public class ShooterConstants {
     public static final double HOOD_MAX_ACCELERATION_LIMIT = 0;
     public static final double POSITION_CONVERSION_FACTOR = 0;
     public static final Gains HOOD_PID_GAINS = new Gains(1, 1, 1);
+    public static final int BEAM_BREAK_CHANNEL = 0;
     public static final TrapezoidProfile.Constraints HOOD_CONSTRAINTS = new TrapezoidProfile.Constraints(HOOD_MAX_VELOCITY_LIMIT, HOOD_MAX_ACCELERATION_LIMIT);
 }

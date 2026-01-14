b07bac4c6755c9270d2ddcc8a4793b60c010ae64
Yehuda Rothstein <129756006+YehudaRothstein@users.noreply.github.com>
2026-01-14 17:12:43 +0200

    added delivery and fixed shoot

--- src/main/java/frc/robot/Subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/Subsystems/Shooter/Shooter.java
@@ -1,6 +1,9 @@
 package frc.robot.Subsystems.Shooter;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -12,11 +15,13 @@ import frc.excalib.control.limits.SoftLimit;
 import frc.excalib.control.motor.controllers.TalonFXMotor;
 import frc.excalib.mechanisms.Mechanism;
 import frc.excalib.mechanisms.fly_wheel.FlyWheel;
+import frc.robot.Constants;
 
 import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 
 import static frc.robot.Subsystems.Shooter.ShooterConstants.*;
+import static frc.robot.Constants.FieldConstants.*;
 
 public class Shooter extends SubsystemBase {
 
@@ -87,15 +92,26 @@ public class Shooter extends SubsystemBase {
         ).until(hasFuel.negate()).withTimeout(0.1);
     }
 
+    public Translation2d velocityAndAngle(Translation3d target){
+        return null;
+    }
+
     public Command shootCommand() {
-        double angle = 0;
-        double flyWheelSpeed = 0;
+        double angle = velocityAndAngle(Constants.FieldConstants.BLUE_HUB_CENTER_POSE).getAngle().getRadians();
+        double flyWheelSpeed = velocityAndAngle(Constants.FieldConstants.BLUE_HUB_CENTER_POSE).getNorm();
         return new SequentialCommandGroup(
                 flyWheelWarmupCommand(flyWheelSpeed),
                 setHoodAngleCommand(() -> angle),
                 getFuelCommand()
         );
     }
-
-
+    public Command deliveryCommand(){
+        double angle = velocityAndAngle(new Translation3d(0,0,0)).getAngle().getRadians();
+        double flyWheelSpeed = velocityAndAngle(new Translation3d(0,0,0)).getNorm();
+        return new SequentialCommandGroup(
+                flyWheelWarmupCommand(flyWheelSpeed),
+                setHoodAngleCommand(() -> angle),
+                getFuelCommand()
+        );
+    }
 }

03275cb052820e4c539aca99e23a5702fba303cf
Software Dev <excalibur@ybalm.co.il>
2026-01-14 12:47:24 +0200

    finished intake commands

--- src/main/java/frc/robot/Subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/Subsystems/Intake/Intake.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems.intake;
 
-import com.ctre.phoenix6.controls.compound.Diff_VoltageOut_Velocity;
 import com.ctre.phoenix6.hardware.CANcoder;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -11,7 +10,6 @@ import frc.excalib.control.motor.controllers.TalonFXMotor;
 import frc.excalib.mechanisms.Arm.Arm;
 import frc.excalib.mechanisms.Mechanism;
 
-import java.util.function.Consumer;
 import java.util.function.DoubleSupplier;
 
 import static frc.robot.subsystems.intake.IntakeConstants.*;
@@ -20,6 +18,9 @@ public class Intake extends SubsystemBase {
 
     public final TalonFXMotor fourBarMotor;
     public final TalonFXMotor rollerMotor;
+    public final TalonFXMotor transportMotor;
+    public final Mechanism transportMotorMechanism;
+    public final Mechanism rollerMotorMechanism;
     public final Arm fourBarMechanism;
     public final SoftLimit armVLimit;
     public final Gains armGains;
@@ -28,13 +29,18 @@ public class Intake extends SubsystemBase {
     public final Mass armMass;
     public final SoftLimit intakeMaxAngle;
     public final Trigger isOpenTrigger;
-    public final Trigger isCloseTrigger;
+    public final Trigger isClosedTrigger;
 
 
     public Intake(){
         angleEncoder = new CANcoder(ANGLE_ENCODER_ID);
         fourBarMotor = new TalonFXMotor(FOUR_BAR_MOTOR_ID);
         rollerMotor = new TalonFXMotor(ROLLER_MOTOR_ID);
+        transportMotor = new TalonFXMotor(TRANSPORT_MOTOR_ID);
+        transportMotorMechanism = new Mechanism(transportMotor);
+        rollerMotorMechanism = new Mechanism(rollerMotor);
+        // rollerMotorMechanism.setVoltage(INTAKE_ROLLER_VOTAGE);
+        // transportMotorMechanism.setVoltage(TRANSPORT_MOTOR_VOLTAGE);
         armVLimit = new SoftLimit(() -> ARM_MIN_V_LIMIT,
                 () -> ARM_MAX_V_LIMIT);
         armGains = new Gains();
@@ -45,7 +51,7 @@ public class Intake extends SubsystemBase {
         intakeMaxAngle = new SoftLimit(()-> INTAKE_MIN_ANGLE,()-> INTAKE_MAX_ANGLE);
         isOpenTrigger = new Trigger(()-> Math.abs( FLOOR_INTAKE_ANGLE - angleEncoder.getPosition().getValueAsDouble()
                 * 2 * Math.PI) < FLOOR_INTAKE_ANGLE_TOLERANCE);
-        isCloseTrigger = new Trigger(()-> Math.abs( CLOSE_INTAKE_ANGLE - angleEncoder.getPosition().getValueAsDouble()
+        isClosedTrigger = new Trigger(()-> Math.abs( CLOSE_INTAKE_ANGLE - angleEncoder.getPosition().getValueAsDouble()
                 * 2 * Math.PI) < CLOSE_INTAKE_ANGLE_TOLERANCE);
         fourBarMechanism = new Arm(fourBarMotor, angleSupplier, armVLimit, armGains, armMass);
     }
@@ -55,7 +61,7 @@ public class Intake extends SubsystemBase {
                 ()-> FLOOR_INTAKE_ANGLE,
                 at -> at = false,
                 MAX_OFFSET,
-                this);
+                this).until(isOpenTrigger);
     }
 
     public Command closeIntakeCommand(){
@@ -63,6 +69,13 @@ public class Intake extends SubsystemBase {
                 ()-> CLOSE_INTAKE_ANGLE,
                 at -> at = false,
                 MAX_OFFSET,
-                this);
+                this).until(isClosedTrigger);
+    }
+
+    public Command intake(){
+        return new SequentialCommandGroup(
+                openIntakeCommand(),
+                rollerMotorMechanism.manualCommand(() -> INTAKE_ROLLER_VOLTAGE,this),
+                transportMotorMechanism.manualCommand(() -> TRANSPORT_MOTOR_VOLTAGE,this));
     }
 }
--- src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/Subsystems/Intake/IntakeConstants.java
@@ -1,21 +1,29 @@
 package frc.robot.subsystems.intake;
 
-import java.util.function.Consumer;
-
 public class IntakeConstants {
+
+    // ==== ID's ==== //
     public static final int FOUR_BAR_MOTOR_ID = 0;
+    public static final int TRANSPORT_MOTOR_ID = 0;
     public static final int ROLLER_MOTOR_ID = 0;
     public static final int ANGLE_ENCODER_ID = 0;
+
+    // ==== Voltages ==== //
+    public static final int INTAKE_ROLLER_VOLTAGE = 0;
+    public static final int TRANSPORT_MOTOR_VOLTAGE = 0;
+
+    // ==== Arm Initialization Constants ==== //
     public static final int ARM_MIN_V_LIMIT = 0;
     public static final int ARM_MAX_V_LIMIT = 0;
     public static final int ARM_MASS_TO_AXIS = 0;
     public static final int ARM_MASS = 0;
-    public static final int INTAKE_ROLLER_VOLTAGE = 0;
+    public static final double MAX_OFFSET = 0;
+    public static final double INTAKE_MAX_ANGLE = 0;
+    public static final double INTAKE_MIN_ANGLE = 0;
+
+    // ==== Intake Angels and Tolerances ==== //
     public static final double FLOOR_INTAKE_ANGLE = 0;
     public static final int CLOSE_INTAKE_ANGLE = 0;
     public static final int FLOOR_INTAKE_ANGLE_TOLERANCE = 0;
     public static final int CLOSE_INTAKE_ANGLE_TOLERANCE = 0;
-    public static final double MAX_OFFSET = 0;
-    public static final double INTAKE_MIN_ANGLE = 0;
-    public static final double INTAKE_MAX_ANGLE = 0;
 }

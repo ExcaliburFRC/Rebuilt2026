80a7686f27f94bb046d862e0f439e6cbb69a1b4e
Yehuda Rothstein <129756006+YehudaRothstein@users.noreply.github.com>
2026-01-14 14:59:16 +0200

    removed unnecessary variables and methods and removed encoder and set a manual command

--- src/main/java/frc/robot/subsystems/transport/Constants.java
+++ src/main/java/frc/robot/subsystems/transport/Constants.java
@@ -3,10 +3,9 @@ package frc.robot.subsystems.transport;
 import frc.excalib.control.gains.Gains;
 
 public class Constants {
-    public static final int DRUM_MOTOR_ID = 0; //TODO assign ID
-    public static final int ENCODER_ID = 0; //TODO assign ID
+    public static final int DRUM_MOTOR_ID = 0;
     public static final Gains GAINS = new Gains();
-    public static final double PIDTOLERANCE = 0; //TODO assign ID
-    public static final double SHOOTING_VOLTAGE = 0; //TODO assign voltage
-    public static final double DEFAULT_VOLTAGE = 0; //TODO assign voltage
+    public static final double PIDTOLERANCE = 0;
+    public static final double SHOOTING_VOLTAGE = 0;
+    public static final double DEFAULT_VOLTAGE = 0;
 }
--- src/main/java/frc/robot/subsystems/transport/Transport.java
+++ src/main/java/frc/robot/subsystems/transport/Transport.java
@@ -3,10 +3,12 @@ package frc.robot.subsystems.transport;
 import com.ctre.phoenix6.hardware.CANcoder;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.excalib.control.limits.ContinuousSoftLimit;
 import frc.excalib.control.motor.controllers.TalonFXMotor;
+import frc.excalib.mechanisms.Mechanism;
 import frc.excalib.mechanisms.turret.Turret;
 
 import java.util.function.DoubleSupplier;
@@ -15,33 +17,13 @@ import static frc.robot.subsystems.transport.Constants.*;
 
 public class Transport extends SubsystemBase {
     private TalonFXMotor drumMotor;
-    private CANcoder drumEncoder;
-    public Turret drumMechanism;
-    DoubleSupplier angleSupplier;
-    public Trigger atPositionTrigger;
-
+    public Mechanism drumMechanism;
 
     public Transport() {
         drumMotor = new TalonFXMotor(DRUM_MOTOR_ID);
-        drumEncoder = new CANcoder(ENCODER_ID);
-        drumMechanism = new Turret(drumMotor,
-                new ContinuousSoftLimit(() -> Double.NEGATIVE_INFINITY, () -> Double.POSITIVE_INFINITY),
-                GAINS, PIDTOLERANCE, angleSupplier);
+        drumMechanism = new Mechanism(drumMotor);
     }
-
     public Command manualCommand(DoubleSupplier output) {
         return drumMechanism.manualCommand(output, this);
-    }
-
-    public Command shootCommand(){
-        return manualCommand(()-> SHOOTING_VOLTAGE);
-    }
-
-    public Command shootCommand(DoubleSupplier output){
-        return manualCommand(output);
-    }
-
-    public Command defaultCommand() {
-        return manualCommand(()->DEFAULT_VOLTAGE);
     }
 }

4db593325872db8699252cfa0388c38034c64ac1
Yehuda Rothstein <yehudarothstein@gmail.com>
2026-01-19 12:50:38 +0200

    moved GameDataClient

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.util.GameDataClient;
 import monologue.Monologue;
 
 public class Robot extends TimedRobot {
--- src/main/java/frc/robot/util/GameDataClient.java
+++ src/main/java/frc/robot/util/GameDataClient.java
@@ -1,4 +1,4 @@
-package frc.robot;
+package frc.robot.util;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.excalib.additional_utilities.Alliance;
